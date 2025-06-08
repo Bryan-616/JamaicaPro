1508c5921c6c7d2275696cc237439a2d23f277dd
Bryan-616 <brcamacho@espe.edu.ec>
2025-06-07 23:24:22 -0500

    Configuración para Render: puerto dinámico

--- app.py
+++ app.py
@@ -195,6 +195,9 @@ def reporte_pdf():
     return send_file(buffer, as_attachment=True, download_name="reporte_jamaica.pdf", mimetype='application/pdf')
 
 
+import os
+
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
